1531747a1f217e4f21b5d656a93f3a4e45d7e1e0
Ryan Stong <rvanstong@gmail.com>
2017-09-01 08:30:03 -0700

    adds in controls for octave and tests play_note method

--- synth.py
+++ synth.py
@@ -1,2 +1,42 @@
+# imports microbit library
+from microbit import *
+
+# imports music library
 import music
 
+# MODELS
+# config values
+# starting octave
+OCTAVE = 4
+DURATION = 1
+
+# VIEW METHODS
+def show_note(note, octave):
+    constructed_note = note + ":" + octave
+    display.scroll(constructed_note)
+
+
+
+# CONTROLLER METHODS
+def play_note(note, octave, duration):
+    constructed_note = str(note) + str(octave) + ":" + str(duration)
+    music.play(constructed_note)
+
+def up_octave(value):
+    OCTAVE += 1
+
+def down_octave(value):
+    OCTAVE -= 1
+
+
+while True:
+    if button_a.is_pressed():
+        down_octave(1)
+    elif button_b.is_pressed():
+        up_octave(1)
+    elif button_a.is_pressed() and button_b.is_pressed():
+        display.show(OCTAVE)
+    else:
+        display.show(Image.ASLEEP)
+
+    play_note("C", OCTAVE, DURATION)
