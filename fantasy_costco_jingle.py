ac3afec00f2329820f844e9a079886d605ada737
Ryan Stong <rvanstong@gmail.com>
2017-09-03 09:36:27 -0700

    finishes fantasy costco theme

--- fantasy_costco_jingle.py
+++ fantasy_costco_jingle.py
@@ -3,11 +3,11 @@ from microbit import *
 # imports music library
 import music
 
-tune = ["E3:1", "E3:1", "E3:1", "D3:2", "G3:2", "E3:1", "F3:2", "E3:1", "D3:1", "C3:2", "C3:2"]
+tune = ["E3:1", "E3:1", "E3:1", "D3:2", "G3:2", "E3:1", "F3:2", "E3:1", "D3:1", "C3:2", "C3:2", "E3:1", "E3:1", "F3:1", "E3:1", "G3:2"]
 
 music.set_tempo(ticks=4, bpm=60)
 while True:
-    if button_a.is_pressed():
+    if button_a.is_pressed() or button_b.is_pressed():
         display.show(Image.HAPPY)
         music.play(tune)
     else:
