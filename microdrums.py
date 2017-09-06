4cb536d871ca8c7e6a52b3d99b9b234d100228f3
Ryan Stong <rvanstong@gmail.com>
2017-09-06 02:49:07 -0700

    adds in frequency of beats to microdrums.py

--- microdrums.py
+++ microdrums.py
@@ -4,14 +4,47 @@ from microbit import *
 # imports music library
 import music
 
+class Drums():
+    def __init__(self):
+        self.frequency_interval = 50
+
+    def freq_up(self):
+        if self.frequency_interval < 400:
+            self.frequency_interval += 50
+        else:
+            self.frequency_interval = 450
+
+    def freq_down(self):
+        if self.frequency_interval > 100:
+            self.frequency_interval -= 50
+        else:
+            self.frequency_interval = 50
+
+
+    def play_drums(self):
+        while True:
+            if button_a.is_pressed():
+                break
+            else:
+                music.play("G0:1")
+                sleep(self.frequency_interval)
+                display.show(Image.HEART)
+                sleep(self.frequency_interval)
+                display.show(Image.HEART_SMALL)
+
+
+
+drums = Drums()
+
 while True:
+    if button_a.is_pressed() and button_b.is_pressed():
+        # plays drums
+        drums.play_drums()
     if button_a.is_pressed():
-        music.play("G0:1")
-        sleep(100)
-        display.show(Image.HEART)
+        # freq down
+        drums.freq_down()
     elif button_b.is_pressed():
-        music.play("G0:1")
-        sleep(300)
-        display.show(Image.HEART)
+        # freq up
+        drums.freq_up()
     else:
         display.show(Image.HEART_SMALL)
