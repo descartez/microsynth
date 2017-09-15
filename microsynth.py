2040629e7d41391fde71c20e27db557f181d2c03
Ryan Stong <rvanstong@gmail.com>
2017-09-15 02:43:49 -0700

    introduces pontentiometer as main note control

--- microsynth.py
+++ microsynth.py
@@ -7,76 +7,35 @@ import music
 class Synth():
     def __init__(self):
         self.octave = 4
-        self.tempo = 240
+        self.sustain = 4
+        self.tempo = 100
 
 
-    def up_octave(self, value):
-        if self.octave < 8:
-            self.octave += value
-        else:
-            self.octave = 9
+    def set_octave(self, value):
+        self.octave = value
 
-    def down_octave(self, value):
-        if self.octave > 1:
-            self.octave -= value
-        else:
-            self.octave = 0
+    def set_sustain(self, value):
+        self.sustain = value
 
     def play_note(self, note):
-        music.play((str(note) + str(self.octave) + ":4"))
+        music.play((str(note) + str(self.octave) + ":" + str(self.sustain)))
 
 synth = Synth()
 
 jingle = ["C4:4","D4:4","E5:4","C6:4"]
+notes = ["C","D","E","F","G","A","B"]
 
-music.set_tempo(ticks=15, bpm=synth.tempo)
+music.set_tempo(ticks=16, bpm=synth.tempo)
 
 # starting jingle
 music.play(jingle)
 
 while True:
     # octave control
-    if button_a.is_pressed() and button_b.is_pressed():
-        display.show(str(synth.octave))
-    elif pin1.read_digital():
-        display.show("A")
-        synth.play_note("A")
-    elif pin2.read_digital():
-        display.show("B")
-        synth.play_note("B")
-    elif pin3.read_digital():
-        display.show("C")
-        synth.play_note("C")
-    elif pin8.read_digital():
-        display.show("D")
-        synth.play_note("D")
-    elif pin12.read_digital():
-        display.show("E")
-        synth.play_note("E")
-    elif pin13.read_digital():
-        display.show("E")
-        synth.play_note("E")
-    elif pin15.read_digital():
-        display.show("F")
-        synth.play_note("F")
-    elif pin16.read_digital():
-        display.show("G")
-        synth.play_note("G")
-    elif pin19.read_digital():
-        #ups tempo
-        display.show(Image.NORTH)
-    elif pin20.read_digital():
-        #downs tempo
-        display.show(Image.SOUTH)
-    elif button_a.is_pressed():
-        synth.down_octave(1)
-        display.show(str(synth.octave))
-        synth.play_note("C")
-        sleep(100)
-    elif button_b.is_pressed():
-        synth.up_octave(1)
-        display.show(str(synth.octave))
-        synth.play_note("C")
-        sleep(100)
-    else:
-        display.show(Image.ASLEEP)
+    note_index = int((pin1.read_analog() / 1023) * 6)
+    display.show(str(note_index))
+    if button_a.is_pressed():
+        synth.play_note(notes[note_index])
+    if button_b.is_pressed():
+        sustain = int((pin1.read_analog() / 1023) * 8)
+        synth.set_sustain(int((pin1.read_analog() / 1023) * 8))
