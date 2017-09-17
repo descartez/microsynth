ec7c05a59959eda114622da5d0a744e96f76daff
Ryan Stong <rvanstong@gmail.com>
2017-09-17 14:58:45 -0700

    enables sharp and flat notes with button press

--- microsynth.py
+++ microsynth.py
@@ -20,25 +20,25 @@ class Synth():
          "B" + str(self.octave),
          ]
 
-    def construct_notes(self):
+    def construct_notes(self, pitch):
         self.notes = [
-         "C" + str(self.octave),
-         "D" + str(self.octave),
-         "E" + str(self.octave),
-         "F" + str(self.octave),
-         "G" + str(self.octave),
-         "A" + str(self.octave),
-         "B" + str(self.octave),
+         "C" + pitch + str(self.octave),
+         "D" + pitch + str(self.octave),
+         "E" + pitch + str(self.octave),
+         "F" + pitch + str(self.octave),
+         "G" + pitch + str(self.octave),
+         "A" + pitch + str(self.octave),
+         "B" + pitch + str(self.octave),
          ]
 
-    def set_octave(self, value):
-        self.octave = value
-        self.construct_notes()
+    def set_octave_pitch(self, octave, pitch=""):
+        self.octave = octave
+        self.construct_notes(pitch)
 
     def set_sustain(self, value):
         self.sustain = value
 
-    def play_note(self, note, pitch=""):
+    def play_note(self, note):
         music.play((str(note) + ":" + str(self.sustain)))
 
     def play_pitch(self, freq, sustain):
@@ -72,17 +72,23 @@ while True:
         synth.switch_mode()
 
     if synth.musical == True:
-        if (pin1.read_analog() > 8):
-            display.show(Image.HEART)
-            music.set_tempo(ticks=16, bpm=tempo)
-            synth.set_octave(octave)
+        if pin2.read_digital():
+            display.show("b")
+            synth.set_octave_pitch(octave, "b")
             synth.play_note(synth.notes[note_index])
         elif pin3.read_digital():
-            synth.play_pitch(pitch, synth.sustain)
+            display.show("#")
+            synth.set_octave_pitch(octave, "#")
+            synth.play_note(synth.notes[note_index])
+        elif pin1.read_analog() > 8:
+            display.show(Image.HEART)
+            music.set_tempo(ticks=16, bpm=tempo)
+            synth.set_octave_pitch(octave)
+            synth.play_note(synth.notes[note_index])
         else:
             display.show(Image.HEART_SMALL)
     else:
-        if (pin1.read_analog() > 8):
+        if pin1.read_analog() > 8:
             display.show(Image.HAPPY)
             synth.play_pitch(pitch, synth.sustain)
         else:
